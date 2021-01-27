0e743819f23c1edabb76217bd65fe672bdb891f8
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-27 03:14:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
+  birthday_kids.each do |age, kids_name|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
   happy_birthday
